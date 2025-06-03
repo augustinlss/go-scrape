b77f643dc1613008131fa84b55baeb5f23f9d274
augustinlss <augustin.zhlass@gmail.com>
2025-06-03 14:14:25 +0200

    Added minor exception handling

--- scraper.go
+++ scraper.go
@@ -1,5 +1,32 @@
 package goscrape
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/augustinlss/goscrape/utils"
+)
+
 func Scrape(url string, depth int16) *ScrapedPage {
+	resp, status, err := utils.Fetch(url)
+
+	defer resp.Body.Close()
+
+	if err != nil {
+		log.Fatalf("Error fetching url content...")
+		return nil
+	}
+
+	if status != http.StatusOK {
+		log.Fatalf("Unexpected status code when fetching url content...")
+		return nil
+	}
+
+	//TODO need to ingest the retuned response package...
+	//
+	// not really sure how i am going to do this.
+	// do i just store all the entire response in a single buffer?
+	// then parse it through some smart html parser?
 
 }
