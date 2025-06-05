f31f187be2226dc8b768c50533af3ae1be435c6b
augustinlss <augustin.zhlass@gmail.com>
2025-06-05 15:06:06 +0200

    Implemented paragraph extraction method

--- extract.go
+++ extract.go
@@ -9,4 +9,13 @@ func ExtractTitle(doc *goquery.Document) string {
 
 }
 
-func ExtractParagraphs(doc *goquery.Document) []string
+func ExtractParagraphs(doc *goquery.Document) []string {
+	paragraphsSelection := doc.Find("p")
+
+	var paragraphs []string
+	paragraphsSelection.Each(func(i int, s *goquery.Selection) {
+		paragraphs = append(paragraphs, s.Text())
+	})
+
+	return paragraphs
+}
